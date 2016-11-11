d30e28fc351f29a368f0d46266f43f255bd83c25
Hernandez Francois <francois.hernandez@supelec.fr>
2016-11-11 18:54:38 +0100

    ex6 ok

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -1,9 +1,9 @@
 function [C, sigma] = dataset3Params(X, y, Xval, yval)
-%EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%DATASET6PARAMS returns your choice of C and sigma for Part 3 of the exercise
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
-%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
-%   sigma. You should complete this function to return the optimal C and 
+%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and
+%   sigma. You should complete this function to return the optimal C and
 %   sigma based on a cross-validation set.
 %
 
@@ -15,17 +15,50 @@ sigma = 0.3;
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
 %               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
+%               validation set. For example,
 %                   predictions = svmPredict(model, Xval);
 %               will return the predictions on the cross validation set.
 %
-%  Note: You can compute the prediction error using 
+%  Note: You can compute the prediction error using
 %        mean(double(predictions ~= yval))
 %
 
+% Create vectors of C and sigma values
 
+C_vect = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+sigma_vect = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
 
+C_opt = 0;
+sigma_opt = 0;
 
+model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+
+predictions = svmPredict(model,Xval);
+
+error_temp = mean(double(predictions ~= yval));
+
+% For loops to go over C and sigma
+
+for i = 1:size(C_vect)
+    for j = 1:size(sigma_vect)
+        C = C_vect(i);
+        sigma = sigma_vect(j);
+        model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+        
+        predictions = svmPredict(model,Xval);
+        
+        error = mean(double(predictions ~= yval));
+        
+        if(error<error_temp)
+            error_temp = error;
+            C_opt = C;
+            sigma_opt = sigma;
+        end
+    end
+end
+
+C = C_opt;
+sigma = sigma_opt;
 
 
 
